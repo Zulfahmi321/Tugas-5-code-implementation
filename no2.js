aa5ba0808597e2b82021049846e3456e0d3f4f11
Zulfahmi321 <zulfahmi1729@gmail.com>
2022-04-20 12:28:49 +0700

    init

--- no2.js
+++ no2.js
@@ -7,16 +7,12 @@ const reverseWords = (kalimat) => {
         if (kalimat[i] !== " ") {           // ketika karakter dari kalimat bukan spasi maka melakukan looping kalimat ke dalam variabel kata
             kata = kata + kalimat[i]
         }
-        if (kalimat[i] === " " && hasil === "") {           // ketika karakter dari kalimat berupa spasi maka memasukan isi variabel kata ke variabel hasil lalu mengkosongkan variabel kata
-            hasil = kata
-            kata = ""
-        }
         if (kalimat[i] === " ") {           // ketika karakter dari kalimat berupa spasi maka memasukan isi variabel kata ke variabel hasil lalu mengkosongkan variabel kata
-            hasil = kata + "0" + hasil
+            hasil = " " + kata + hasil
             kata = ""
         }
     }
-    hasil = kata + " " + hasil  // kata terakhir pada kalimat tidak ada spasi, untuk menampung kata terakhir kedalam hasil
+    hasil = kata + hasil  // kata terakhir pada kalimat tidak ada spasi, untuk menampung kata terakhir kedalam hasil
     return hasil;
 }
 
